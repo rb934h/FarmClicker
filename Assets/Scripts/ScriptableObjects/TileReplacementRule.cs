b1990b31448aae9258cc13d5ad8afd5a2bfcca82
ValeriiAndreev-azur <regulusblack934@gmail.com>
2025-08-23 06:13:57 +0400

    feat: add tile replacement rules

---
+++ Assets/Scripts/ScriptableObjects/TileReplacementRule.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace ScriptableObjects
+{
+    [CreateAssetMenu(fileName = "NewTileReplacementRule", menuName = "TileReplacementRules/TileReplacementRule")]
+    public class TileReplacementRule : ScriptableObject
+    {
+        public TileBase[] from;
+        public TileBase[] to;
+    }
+}
---
+++ Assets/Scripts/TileChanger.cs
@@ -0,0 +1,30 @@
+﻿using ScriptableObjects;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace DefaultNamespace
+{
+    public class TileChanger : MonoBehaviour
+    {
+        [SerializeField] private Tilemap tilemap;
+        [SerializeField] private TileReplacementRule _rule;
+
+        private bool reverse;
+
+
+        public void ChangeTiles()
+        {
+            for (int i = 0; i < _rule.from.Length; i++)
+            {
+                if (reverse)
+                    tilemap.SwapTile(_rule.from[i], _rule.to[i]);
+                else
+                {
+                    tilemap.SwapTile(_rule.to[i], _rule.from[i]);
+                }
+            }
+            
+            reverse = !reverse;
+        }
+    }
+}
