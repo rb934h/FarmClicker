603496fa81549ce99456845614ca8e99f03e91fa
ValeriiAndreev-azur <regulusblack934@gmail.com>
2025-10-14 19:12:57 +0400

    feat: add sorting order

---
+++ Assets/Scripts/SortingOrderManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class SortingOrderManager : MonoBehaviour
+{
+    [SerializeField] private List<SpriteRenderer> _renderers = new();
+    
+    private void LateUpdate()
+    {
+        foreach (var r in _renderers)
+        {
+            r.sortingOrder = Mathf.RoundToInt(-r.transform.position.y * 100);
+        }
+    }
+}
